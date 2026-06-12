e51d7907f9781ff705aec3e236ef61ae5de4aac7
Breno santos <Breno.ds263@gmail.com>
2026-06-12 14:03:51 -0300

    Pratica 6 - parte 3

--- app/src/main/java/com/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/weatherapp/MainViewModel.kt
@@ -1,42 +1,46 @@
 package com.weatherapp
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.model.LatLng
+import com.weatherapp.db.fb.FBCity
+import com.weatherapp.db.fb.FBDatabase
+import com.weatherapp.db.fb.FBUser
+import com.weatherapp.db.fb.toFBCity
 import com.weatherapp.model.City
 import com.weatherapp.model.User
 
-class MainViewModel : ViewModel() {
-
-    private val _user = mutableStateOf<User?>(null)
-
-    val user: User?
-        get() = _user.value
-
-    private val _cities = createCities().toMutableStateList()
-
+class MainViewModel (private val db: FBDatabase): ViewModel(),
+    FBDatabase.Listener {
+    private val _cities = mutableStateListOf<City>()
     val cities
         get() = _cities.toList()
-
+    private val _user = mutableStateOf<User?> (null)
+    val user : User?
+        get() = _user.value
+    init {
+        db.setListener(this)
+    }
     fun remove(city: City) {
-        _cities.remove(city)
+        db.remove(city.toFBCity())
     }
-
-    fun add(name: String) {
-        _cities.add(City(name = name))
+    fun add(name: String, location : LatLng? = null) {
+        db.add(City(name = name, location = location).toFBCity())
     }
-
-    private fun createCities() = List(0
-    ) { i ->
-        City(
-            name = "Cidade $i",
-            weather = "Carregando clima..."
-        )
+    override fun onUserLoaded(user: FBUser) {
+        _user.value = user.toUser()
     }
-
-    fun add(name: String, location: LatLng? = null) {
-        _cities.add(City(name = name, location = location))
-
+    override fun onUserSignOut() {
+        //TODO("Not yet implemented")
+    }
+    override fun onCityAdded(city: FBCity) {
+        _cities.add(city.toCity())
+    }
+    override fun onCityUpdated(city: FBCity) {
+        //TODO("Not yet implemented")
+    }
+    override fun onCityRemoved(city: FBCity) {
+        _cities.remove(city.toCity())
     }
 }
