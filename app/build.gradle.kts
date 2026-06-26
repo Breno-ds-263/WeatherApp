a5303939b5612af1b5bf743a43d9615e4a4b344a
Breno santos <Breno.ds263@gmail.com>
2026-06-26 15:16:20 -0300

    Pratica 7 - parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
         alias(libs.plugins.android.application)
         alias(libs.plugins.kotlin.android)
@@ -13,11 +15,19 @@ plugins {
         compileSdk = 36
 
         defaultConfig {
+
+            val keyFile = project.rootProject.file("local.properties")
+            val props = Properties()
+            props.load(keyFile.inputStream())
+            buildConfigField ("String", "WEATHER_API_KEY",
+                props.getProperty("WEATHER_API_KEY"))
+
             applicationId = "com.weatherapp"
             minSdk = 24
             targetSdk = 36
             versionCode = 1
             versionName = "1.0"
+
 
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         }
@@ -40,15 +50,17 @@ plugins {
         }
         buildFeatures {
             compose = true
+            buildConfig = true
+
         }
     }
 
     dependencies {
-        // Google maps
         implementation("com.google.android.gms:play-services-maps:20.0.0")
         implementation("com.google.android.gms:play-services-location:21.3.0")
-        // Google maps for compose
         implementation("com.google.maps.android:maps-compose:8.3.0")
+        implementation ("com.squareup.retrofit2:retrofit:3.0.0")
+        implementation ("com.squareup.retrofit2:converter-gson:3.0.0")
 
         implementation("androidx.navigation:navigation-compose:2.9.8")
         implementation(libs.firebase.auth)
