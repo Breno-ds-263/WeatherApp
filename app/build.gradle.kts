59bce475ad13e481fd8e86b316fa53f410aec80d
Breno santos <Breno.ds263@gmail.com>
2026-07-10 01:43:35 -0300

    Pratica 9 - parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ plugins {
         implementation("com.google.maps.android:maps-compose:8.3.0")
         implementation ("com.squareup.retrofit2:retrofit:3.0.0")
         implementation ("com.squareup.retrofit2:converter-gson:3.0.0")
+        implementation("io.coil-kt:coil-compose:2.7.0") // coil
+
 
         implementation("androidx.navigation:navigation-compose:2.9.8")
         implementation(libs.firebase.auth)
--- app/src/main/java/com/weatherapp/HomePage.kt
+++ app/src/main/java/com/weatherapp/HomePage.kt
@@ -21,10 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.weatherapp.model.Forecast
 import java.text.DecimalFormat
 
@@ -59,11 +61,13 @@ fun HomePage(
 
             Row {
 
-                Icon(
-                    imageVector = Icons.Filled.AccountBox,
-                    contentDescription = null,
-                    modifier = Modifier.size(150.dp)
+                AsyncImage( // Substitui o Icon
+                    model = viewModel.weather(viewModel.city!!).imgUrl,
+                    modifier = modifier.size(140.dp),
+                    error = painterResource(id = R.drawable.loading),
+                    contentDescription = "Imagem"
                 )
+
 
                 Column {
 
@@ -138,10 +142,11 @@ fun ForecastItem(
         verticalAlignment = Alignment.CenterVertically
     ) {
 
-        Icon(
-            imageVector = Icons.Filled.LocationOn,
-            contentDescription = null,
-            modifier = Modifier.size(48.dp)
+        AsyncImage( // Substitui o Icon
+            model = forecast.imgUrl,
+            modifier = modifier.size(70.dp),
+            error = painterResource(id = R.drawable.loading),
+            contentDescription = "Imagem"
         )
 
         Spacer(modifier = Modifier.size(16.dp))
